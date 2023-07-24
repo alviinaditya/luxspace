e58ae73591154cf4bfcbeec7186f54465593b945
Alvin A. Rahman <alviinaditya@gmail.com>
2023-07-24 12:45:28 +0700

    add crud product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
@@ -16,9 +18,19 @@ class ProductController extends Controller
         if (request()->ajax()) {
             $query = Product::query();
             return DataTables::of($query)
+                ->addColumn('action', function ($item) {
+                    return '
+                        <a class="inline-block bg-gray-500 text-white rounded-md px-2 py-1 m-2" href="' . route('dashboard.product.edit', $item->id) . '">Edit</a>
+                        <form class="inline-block" action="' . route('dashboard.product.destroy', $item->id) . '" method="POST">
+                            ' . method_field('delete') . csrf_field() . '
+                            <button class="bg-red-500 text-white rounded-md px-2 py-1 m-2">Hapus</button>
+                        </form>
+                    ';
+                })
                 ->editColumn('price', function ($item) {
                     return "Rp. " . number_format($item->price, 0, ',', '.') . ",-";
                 })
+                ->rawColumns(['action'])
                 ->make();
         }
         return view('pages.dashboard.product.index');
@@ -29,15 +41,20 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.dashboard.product.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $data = $request->all();
+        $data['slug'] = Str::slug($request->name);
+
+        Product::create($data);
+
+        return redirect()->route('dashboard.product.index');
     }
 
     /**
@@ -51,24 +68,30 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Product $product)
     {
-        //
+        return view('pages.dashboard.product.edit', compact('product'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(ProductRequest $request, Product $product)
     {
-        //
+        $data = $request->all();
+        $data['slug'] = Str::slug($request->name);
+
+        $product->update($data);
+
+        return redirect()->route('dashboard.product.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return redirect()->route('dashboard.product.index');
     }
 }
